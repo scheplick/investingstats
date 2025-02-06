260354d70f08e1ec9410573281ff54716810ab84
Stefan Cheplick <stefan.cheplick@gmail.com>
2025-02-06 16:50:44 -0500

    Create investingstats-template.php

---
+++ investingstats-template.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+Template Name: InvestingStats Blank Page
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>InvestingStats</title>
+    <style>
+        body {
+            background: white;
+            color: black;
+            font-size: 24px;
+            text-align: center;
+            margin-top: 20%;
+        }
+    </style>
+</head>
+<body>
+    Hello World!
+</body>
+</html>
